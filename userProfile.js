714d20aac208453542658356fb88a81369aaefc7
unknown <nshienarocell@gmail.com>
2024-11-04 18:00:44 +0800

    initial commit

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullName = "Shiena Rocell M. Naungayan";
+let age = 20;
+let favoriteNumber = 2;
+let favoriteColor = "black";
+
+console.log(fullName);
+console.log(age);
+console.log(favoriteNumber);
+console.log(favoriteColor);
